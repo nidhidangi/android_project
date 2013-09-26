6af6c8bbb5c09dc71b753482836e19ed81e90680
puneet rangnekar <puneet.rangnekar@vvdntech.com>
2013-09-26 15:51:47 +0530

    new changes
    executed

--- BottomTabsFragmentTabHost-master/src/com/ramesh/fragmenttabhostdemo/Fragment2.java
+++ BottomTabsFragmentTabHost-master/src/com/ramesh/fragmenttabhostdemo/Fragment2.java
@@ -1,6 +1,17 @@
 package com.ramesh.fragmenttabhostdemo;
 
+import android.os.Bundle;
 
-public class Fragment2 extends Fragment1 {
+
+public class Fragment2 extends Fragment1
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	
+	System.out.println("Workingg finee!!!!");
+	
+	}
 
 }
